0f828891284d99dffe954011385ac2862e6b1349
Lucas <lucas.fc@defensoria.to.gov.br>
2015-04-29 14:49:11 -0300

    Publicando Gem

---
+++ lib/seraph_auth.rb
@@ -0,0 +1,60 @@
+class SeraphAuth < ActiveRecord::Base
+
+	def self.url
+		"http://seraph.defensoria.to.gov.br/"	
+	end
+
+	def self.login(code)
+		params = {
+			grant_type: 'authorization_code',
+			client_id: ENV['seraph_client'],
+			client_secret: ENV['seraph_secret'],
+			code: code
+		}		
+		begin 
+			response_token = JSON.parse(
+				RestClient.post("#{url}oauth2/access_token/", params), 
+				symbolize_names: true
+			)
+			response  = JSON.parse(
+				RestClient.get(
+					"#{url}api/profile/", 
+					{:Authorization => "Bearer #{response_token[:access_token]}" }
+				), 
+				symbolize_names: true
+			)
+
+			login = response[:username]
+			cpf = response[:cpf]
+			email = response[:email]
+			nome = response[:first_name]
+			matricula = response[:matricula]
+
+			return usuario_login(login,cpf,email, nome, matricula)
+		rescue
+			return {error: true}
+		end
+	end
+
+	def self.usuario_login(login,cpf,email, nome, matricula)
+		user = User.find_by_username(login)
+		if !user.blank?
+			return user
+		else
+			return create_usuario(login,cpf,email,nome, matricula)
+		end
+	end
+
+	def self.create_usuario(login,cpf,email,nome=nil,matricula=nil)
+		senha = SecureRandom.urlsafe_base64(nil, false)
+		user = ENV['seraph_table'].constantize.new
+		user.send(ENV['seraph_column_email']) = email
+		user.send(ENV['seraph_column_name']) = nome
+		user.send(ENV['seraph_column_cpf']) = cpf
+		user.send(ENV['seraph_column_usermame']) = login
+		user.send(ENV['seraph_column_password']) = senha
+		user.send(ENV['seraph_column_confirmation']) = senha
+		user.save
+		return user
+	end
+end
---
+++ seraph_auth.gemspec
@@ -0,0 +1,18 @@
+# coding: utf-8
+lib = File.expand_path('../lib', __FILE__)
+$LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
+require 'athenas_api_client/version'
+
+Gem::Specification.new do |spec|
+  spec.name          = "SERAPH Auth"
+  spec.version       = "1.0"
+  spec.authors       = ["Lucas Ferreira"]
+  spec.email         = ["lucas.ferreira@outlook.com"]
+  spec.summary       = 'OAuth com SERAPH'
+  spec.homepage      = "gitlab.defensoria.to.gov.br"
+  spec.license       = "MIT"
+
+  spec.files         = ["lib/seraph_auth.rb"] 
+
+  spec.add_dependency 'rest-client'
+end
