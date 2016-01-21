4697e68e6a46118d5bc98a77a649b9cfb94f24cf
Lucas Ferreira <lucas.ferreira@outlook.com>
2016-01-21 18:52:16 -0200

    try fix

--- lib/generators/toauth_connect_generator.rb
+++ lib/generators/toauth_connect_generator.rb
@@ -8,4 +8,8 @@ class ToauthConnectGenerator < Rails::Generators::Base
   def copy_initializer
     template "toauth_connect.rb", "config/initializers/toauth_connect.rb"
   end
+
+  def copy_controller
+    template "toauth_controller.rb", "app/controllers/toauth_contorller.rb"
+  end
 end
