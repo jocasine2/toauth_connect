64fdda0ab2cd411900ea17d03628b24987f6271c
Lucas Ferreira <lucas.ferreira@outlook.com>
2016-01-21 18:32:47 -0200

    try fix

---
+++ lib/generators/initializer_generator.rb
@@ -0,0 +1,15 @@
+require 'rails/generators/base'
+module ToauthConnect
+  module Generators
+    class InstallGenerator < Rails::Generators::Base
+      source_root File.expand_path("../../templates", __FILE__)
+
+      desc "Creates a TouthConnect initializer and copy locale files to your application."
+      class_option :orm
+
+      def copy_initializer
+        template "toauth_connect.rb", "config/initializers/toauth_connect.rb"
+      end
+    end
+  end
+end
---
+++ lib/generators/template/toauth_connect.rb
@@ -0,0 +1,14 @@
+ToauthConnect.configuration do |config|
+  config.client_id  = "client_id"
+  config.client_secret  = "client_secret"
+  config.domain_default  = "domain_default"
+  config.url_redirect_production  = "url_redirect_production"
+  config.url_redirect_dev  = "url_redirect_dev"
+  config.user_table  = "user_table"
+  config.column_email  = "column_email"
+  config.colum_token  = "colum_token"
+  config.column_name  = "column_name"
+  config.column_cpf  = "column_cpf"
+  config.column_password  = "column_password"
+  config.column_password_confirmation  = "column_password_confirmation"
+end
