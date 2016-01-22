46cbf256ced32ebefedf658b19a2820e5ba05afe
Lucas Ferreira <lucas.ferreira@outlook.com>
2016-01-22 10:49:22 -0200

    automatic toauth tester

--- lib/generators/template/toauth_connect.rb
+++ lib/generators/template/toauth_connect.rb
@@ -4,11 +4,11 @@ ToauthConnect.configuration do |config|
   config.domain_default  = "domain_default" #Domnio padrão
   config.url_redirect_production  = "url_redirect_production" #URL de Redirecionamento em produção
   config.url_redirect_dev  = "http://localhost:3000/toauth" #Url de redirecionamento em desenvolvimento
-  config.user_table  = "user_table" #Nome da tabela contendo o Usuário
-  config.column_email  = "column_email" #Coluna da tabela contendo o email
-  config.column_token  = "colum_token" #Coluna da tabela contendo o token
-  config.column_name  = "column_name" #Coluna da tabela contendo o nome
-  config.column_cpf  = "column_cpf" #Coluna da tabela contendo o cpf
-  config.column_password  = "column_password" #Coluna da tabela contendo o password
-  config.column_password_confirmation  = "column_password_confirmation" #Coluna da tabela contendo a confirmação de password
+  config.user_table  = "User" #Nome da tabela contendo o Usuário
+  config.column_email  = "email" #Coluna da tabela contendo o email
+  config.column_token  = "token" #Coluna da tabela contendo o token
+  config.column_name  = "name" #Coluna da tabela contendo o nome
+  config.column_cpf  = "cpf" #Coluna da tabela contendo o cpf
+  config.column_password  = "password" #Coluna da tabela contendo o password
+  config.column_password_confirmation  = "password_confirmation" #Coluna da tabela contendo a confirmação de password
 end
