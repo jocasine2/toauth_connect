66604f76ee476e6a2d63d23936189eb4476f8fe6
Lucas Ferreira <lucas.ferreira@outlook.com>
2016-01-22 11:21:37 -0200

    automatic toauth tester

---
+++ lib/generators/auto_generator.rb
@@ -0,0 +1,45 @@
+require 'rails/generators'
+module ToauthConnect
+  module Generators
+    class AutoGenerator < Rails::Generators::Base
+      def add_gens
+        gem 'devise'
+        gem 'oauth2'
+        gem 'rest-client'
+      end
+      
+      def create_user_table
+        generate "scaffold", "user name cpf token"
+      end
+
+      def install_devise
+         generate "devise:install"
+      end
+
+      def install_devise
+         generate "devise",  "user"
+      end
+
+      def run_rake
+        rake "db:migrate"
+      end
+
+      def generate_toauth_connect
+        generate "toauth_connect"
+      end
+
+      def add_user_method
+        inject_into_class 'app/model/user.rb', "User", <<-'RUBY'
+          def toauth_fast
+            ToauthConnect.fastdata(self)
+          end
+
+          def toauth_data
+            ToauthConnect.profile_data(self)
+          end
+        RUBY
+        end
+      end
+    end
+  end
+end
